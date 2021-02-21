dd2388a89601a77c72a160199753c9dc1d89c686
Infarh <shmachilin@yandex.ru>
2021-02-21 11:24:49 +0300

    Hosting

--- ReflectionEditDialog/App.xaml.cs
+++ ReflectionEditDialog/App.xaml.cs
@@ -1,6 +1,47 @@
-﻿namespace ReflectionEditDialog
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ReflectionEditDialog
 {
     public partial class App
     {
+        public static Window FocusedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+
+        public static Window ActiveWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+
+        public static Window CurrentWindow => FocusedWindow ?? ActiveWindow ?? Current.MainWindow;
+
+        private static IHost _Hosting;
+        public static IHost Hosting => _Hosting
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+           .ConfigureAppConfiguration(opt => opt.AddJsonFile("appsettings.json", false, true))
+           .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Hosting;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Hosting;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
