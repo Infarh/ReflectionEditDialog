c4e47e987b74b9900f9990c636fe51a5dd147d8d
Infarh <shmachilin@yandex.ru>
2021-02-21 11:33:10 +0300

    Модель-представления главного окна

---
+++ ReflectionEditDialog/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReflectionEditDialog.ViewModels.Base;
+
+namespace ReflectionEditDialog.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Заголовок окна";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
