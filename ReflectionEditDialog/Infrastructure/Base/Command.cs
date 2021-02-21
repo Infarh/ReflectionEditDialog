ddc8ab86c8f9963a1de035fb700063552ae2a5d6
Infarh <shmachilin@yandex.ru>
2021-02-21 11:17:50 +0300

    Лямда-команда

--- ReflectionEditDialog/Infrastructure/Base/Command.cs
+++ ReflectionEditDialog/Infrastructure/Base/Command.cs
@@ -3,7 +3,7 @@ using System.Windows.Input;
 
 namespace ReflectionEditDialog.Infrastructure.Base
 {
-    internal abstract class Command : ICommand
+    public abstract class Command : ICommand
     {
         public event EventHandler CanExecuteChanged
         {
