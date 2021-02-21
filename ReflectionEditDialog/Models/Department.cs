48abb7aaf273f25059423787280b32c8c9cb9dd5
Infarh <shmachilin@yandex.ru>
2021-02-21 18:29:18 +0300

    Интерфейсы для сравнения сущностей

--- ReflectionEditDialog/Models/Department.cs
+++ ReflectionEditDialog/Models/Department.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ReflectionEditDialog.Models
 {
-    class Department
+    class Department : IEquatable<Department>
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
         public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+        public bool Equals(Department other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Department) obj);
+        }
+
+        public override int GetHashCode() => Id;
+
+        public static bool operator ==(Department left, Department right) => Equals(left, right);
+        public static bool operator !=(Department left, Department right) => !Equals(left, right);
     }
 }
--- ReflectionEditDialog/Models/Employee.cs
+++ ReflectionEditDialog/Models/Employee.cs
@@ -2,7 +2,7 @@
 
 namespace ReflectionEditDialog.Models
 {
-    class Employee
+    class Employee : IEquatable<Employee>
     {
         public int Id { get; set; }
 
@@ -15,5 +15,22 @@ namespace ReflectionEditDialog.Models
         public DateTime Birthday { get; set; }
 
         public Department Department { get; set; }
+
+        public bool Equals(Employee other) => 
+            other is not null 
+            && (ReferenceEquals(this, other) || Id == other.Id);
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Employee) obj);
+        }
+
+        public override int GetHashCode() => Id;
+
+        public static bool operator ==(Employee left, Employee right) => Equals(left, right);
+        public static bool operator !=(Employee left, Employee right) => !Equals(left, right);
     }
 }
