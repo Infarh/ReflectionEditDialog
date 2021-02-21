38701fc2adae3cf8bb566ca39b3082ee3c6f43f7
Infarh <shmachilin@yandex.ru>
2021-02-21 13:48:09 +0300

    Конвертер

---
+++ ReflectionEditDialog/Infrastructure/Converters/Base/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ReflectionEditDialog.Infrastructure.Converters.Base
+{
+    internal abstract class Converter : IValueConverter
+    {
+        public object Convert(object v, Type t, object p, CultureInfo c) => Convert(v);
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c) => ConvertBack(v);
+
+        protected abstract object Convert(object v);
+
+        protected virtual object ConvertBack(object v) => throw new NotSupportedException();
+    }
+}
---
+++ ReflectionEditDialog/Infrastructure/Converters/DateTimeToAgeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ReflectionEditDialog.Infrastructure.Converters.Base;
+
+namespace ReflectionEditDialog.Infrastructure.Converters
+{
+    internal class DateTimeToAgeConverter : Converter
+    {
+        protected override object Convert(object v)
+        {
+            if (v is null) return null;
+            var time = v as DateTime? ?? System.Convert.ToDateTime(v);
+            return (int)Math.Ceiling((DateTime.Now - time).TotalDays / 365);
+        }
+
+        protected override object ConvertBack(object v)
+        {
+            if (v is null) return null;
+            var age = v as int? ?? System.Convert.ToInt32(v);
+
+            return DateTime.Now.Date.AddYears(-age);
+        }
+    }
+}
